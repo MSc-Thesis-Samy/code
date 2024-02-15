1cabdd75a5727e474948d5d7cad5fff32034b40b
samyhaff <haffoudhisamy@gmail.com>
2024-02-15 12:03:58 +0100

    added half example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use neuroevolution::one_plus_one_na::*;
+use std::f64::consts::PI;
 
-fn evaluation_function<const N: usize>(network: &Network<N>) -> f64 {
-    1.
+const STEPS: u32 = 1000;
+
+fn half<const N: usize>(network: &Network<N>) -> f64 {
+    let mut sum = 0;
+    for i in 0..STEPS {
+        let angle = 2. * PI * i as f64 / STEPS as f64;
+        let output = network.evaluate((1., angle));
+        if output && angle < PI || !output && angle > PI {
+            sum += 1;
+        }
+    }
+    sum as f64 / STEPS as f64
 }
 
 fn main() {
     let mut network = Network::<1>::new();
-    network.optimize(evaluation_function, 10);
-    println!("{:?}", network);
+    network.optimize(half, 1000);
+    println!("{}", network);
+    println!("Fitness: {}", half(&network));
 }
--- src/one_plus_one_na.rs
+++ src/one_plus_one_na.rs
@@ -1,9 +1,13 @@
+use std::fmt;
 use rand::prelude::*;
+use rand_distr::{Exp, Distribution};
+use std::f64::consts::PI;
 
 #[derive(Debug, Clone)]
 pub struct Network<const N: usize> {
-    angles: [f64; N],
-    biases: [f64; N],
+    pub angles: [f64; N],
+    pub biases: [f64; N],
+    pub output_layer: fn(&[bool; N]) -> bool,
 }
 
 fn generate_random_array<const N: usize>() -> [f64; N] {
@@ -14,33 +18,66 @@ fn generate_random_array<const N: usize>() -> [f64; N] {
     array
 }
 
+fn polar_dot_product((r1, theta1): (f64, f64), (r2, theta2): (f64, f64)) -> f64 {
+    r1 * r2 * (theta1 - theta2).cos()
+}
+
+impl<const N: usize> std::fmt::Display for Network<N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Network {{ angles: [{}], biases: [{}] }}",
+            self.angles
+                .iter()
+                .map(|&x| format!("{:.2}", 2. * PI * x))
+                .collect::<Vec<_>>()
+                .join(", "),
+            self.biases
+                .iter()
+                .map(|&x| format!("{:.2}", x))
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
+}
+
 impl<const N: usize> Network<N> {
     pub fn new() -> Self {
         Self {
             angles: generate_random_array(),
             biases: generate_random_array(),
+            output_layer: |inputs: &[bool; N]| inputs.iter().any(|&x| x), // OR
         }
     }
 
     pub fn optimize(&mut self, evaluation_function: fn(&Network<N>) -> f64, n_iters: u32) {
+        let exp = Exp::new(1.).unwrap();
+
         for _ in 0..n_iters {
             let mut new_network = self.clone();
-
             for i in 0..N {
                 if random::<f64>() < 1. / (2. * N as f64) {
                     let sign = if random::<f64>() < 0.5 { 1. } else { -1. };
-                    new_network.angles[i] += sign * random::<f64>();
-                    new_network.biases[i] += sign * random::<f64>();
+                    new_network.angles[i] += sign * exp.sample(&mut thread_rng());
+                    new_network.biases[i] += sign * exp.sample(&mut thread_rng());
                     new_network.angles[i] -= new_network.angles[i].floor();
                     new_network.biases[i] -= new_network.biases[i].floor();
                 }
             }
 
-            println!("{:?}", new_network);
-
             if evaluation_function(&new_network) > evaluation_function(self) {
                 *self = new_network;
             }
         }
+    }
+
+    pub fn evaluate(&self, input: (f64, f64)) -> bool {
+        let mut hidden = [false; N];
+        for i in 0..N {
+            let neuron_angle = self.angles[i] * 2. * PI;
+            let normal = (1., self.angles[i] * 2. * PI);
+            hidden[i] = polar_dot_product(input, normal) - self.biases[i] > 0.;
+        }
+        (self.output_layer)(&hidden)
     }
 }
